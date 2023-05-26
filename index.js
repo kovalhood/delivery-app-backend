c7fc248941f764a9c8b84add72ed00734916b37a
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-05-26 19:12:58 +0300

    Added cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const shopsData = require("./database/shops.json");
 let port = 4000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send(`This is a backend API for CoolDelivery shops. Just add "/shops" to the address bar to get data`);
