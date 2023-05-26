5b5484567370aeb5a3d4aee75668d3ab1e6dd959
Valentyn Kovalchuk <90193930+kovalhood@users.noreply.github.com>
2023-05-26 07:48:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const shopsData = require("./database/shops.json");
 let port = 4000;
 
 app.get("/", (req, res) => {
-    res.send(`This is a backend API for CoolDelivery shops. Just add "/shops" for getting data`);
+    res.send(`This is a backend API for CoolDelivery shops. Just add "/shops" to the address bar to get data`);
 })
 
 app.get("/shops", (req, res) => {
